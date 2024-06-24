7f005574e9f9fd1c1ffefe117690d832cc20060f
alexander784 <alexandernyaga1999@gmail.com>
2024-06-24 11:50:57 +0300

    Add Resume model

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 
@@ -11,4 +12,18 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
 
-    
+
+class Resume(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    profile = db.Column(db.Text, nullable=False)
+    experience = db.Column(db.Text, nullable=False)
+    education = db.Column(db.Text, nullable=False)
+    skills = db.Column(db.Text, nullable=False)
+    certificates = db.Column(db.Text, nullable=False)
+    projects = db.Column(db.Text, nullable=False)
+    languages = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref=db.backref('resumes', lazy=True))
+
