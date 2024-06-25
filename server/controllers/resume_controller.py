1c236282683ab23713ee70aefac0ca7f5b4a98a5
alexander784 <alexandernyaga1999@gmail.com>
2024-06-25 11:05:47 +0300

    Handle resume update route

--- server/controllers/resume_controller.py
+++ server/controllers/resume_controller.py
@@ -71,10 +71,7 @@ class ResumeResource(Resource):
             }), 201)
         except Exception as e:
             return make_response(jsonify({"error": str(e)}), 500)
-        
 
-
-##Retrive all resumes
     @jwt_required()
     def get(self):
         user_identity = get_jwt_identity()
@@ -86,14 +83,11 @@ class ResumeResource(Resource):
         resumes = Resume.query.all()
         resume_schema = ResumeSchema(many=True)
         response = resume_schema.dump(resumes)
-        
-
 
         return make_response(jsonify({
             "resumes": response
         }), 200)
-    
-# Delete a resume
+
 @resume_ns.route('/resume/<int:id>')
 class ResumeDetailResource(Resource):
     @jwt_required()
@@ -103,23 +97,55 @@ class ResumeDetailResource(Resource):
             user = User.query.filter_by(username=user_identity).first()
 
             if not user:
-                return make_response(jsonify({"message":"User not found"})), 404
+                return make_response(jsonify({"message":"User not found"}), 404)
             
             resume = Resume.query.filter_by(id=id, user_id=user.id).first()
 
             if not resume:
-                return make_response(jsonify({"message":"Resume not found or authorised "})), 494
+                return make_response(jsonify({"message":"Resume not found or authorized"}), 404)
             
-
             db.session.delete(resume)
             db.session.commit()
 
-            make_response(jsonify({"message":"Resume deleted susccessfully"}), 200)
+            return make_response(jsonify({"message":"Resume deleted successfully"}), 200)
 
         except Exception as e:
             return make_response(jsonify({"error":str(e)}), 500)
-        
 
+    @jwt_required()
+    def patch(self, id):
+        data = request.get_json()
+        try:
+            user_identity = get_jwt_identity()
+            user = User.query.filter_by(username=user_identity).first()
 
+            if not user:
+                return make_response(jsonify({"message":"User not found"}), 404)
+            
+            resume = Resume.query.filter_by(id=id, user_id=user.id).first()
 
+            if not resume:
+                return make_response(jsonify({"message":"Resume not found or authorized"}), 404)
+            
+            # Update fields
+            resume.name = data.get("name", resume.name)
+            resume.profile= data.get("profile", resume.profile)
+            resume.experience = data.get("experience", resume.experience)
+            resume.education = data.get("education", resume.education)
+            resume.skills = data.get("skills", resume.skills)
+            resume.certificates = data.get("certificates", resume.certificates)
+            resume.projects = data.get("projects", resume.projects)
+            resume.languages = data.get("languages", resume.languages)
+            resume.updated_at = datetime.utcnow()
 
+            db.session.commit()
+
+            resume_schema = ResumeSchema()
+            response = resume_schema.dump(resume)
+
+            return make_response(jsonify({
+                "message": "Resume updated successfully",
+                "resume": response
+            }), 200)
+        except Exception as e:
+            return make_response(jsonify({"error": str(e)}), 500)
