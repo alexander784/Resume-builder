441ad5bac2220e729353a9e436c144b2807a4234
alexander784 <alexandernyaga1999@gmail.com>
2024-06-25 13:14:05 +0300

    Structure the NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    Alexander
+    <Router>
+       <Navbar />
+    </Router>
     </>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center">
+          <img src="/path-to-your-logo.png" alt="Logo" className="h-10 mr-3" />
+          <span className="text-white text-xl font-bold">Ralphy</span>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
