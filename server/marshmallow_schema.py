3b75712d0e441d835a4e5c8338e92dc205feaa44
alexander784 <alexandernyaga1999@gmail.com>
2024-07-02 11:48:40 +0300

    Serialize experience and Education

--- server/marshmallow_schema.py
+++ server/marshmallow_schema.py
@@ -16,6 +16,17 @@ class UserSchema(ma.Schema):
 user_schema = UserSchema()
 users_schema = UserSchema(many=True)
 
+class ExperienceSchema(ma.Schema):
+    company = fields.Str(required=True)
+    position=fields.Str(required=True)
+    duration=fields.Str(required=True)
+    description=fields.Str(required=True)
+
+class EducationSchema(ma.Schema):
+    school = fields.Str(required=True)
+    degree=fields.Str(required=True)
+    duration=fields.Str(required=True)
+
 
 class ResumeSchema(ma.Schema):
     class Meta:
@@ -24,8 +35,8 @@ class ResumeSchema(ma.Schema):
     id = fields.Integer(dump_only=True)
     name = fields.Str(required=True)
     profile = fields.Str(required=True)
-    experience = fields.Str(required=True)
-    education = fields.Str(required=True)
+    experience = fields.List(fields.Nested(ExperienceSchema),required=True)
+    education = fields.List(fields.Nested(EducationSchema),required=True)
     skills = fields.Str(required=True)
     certificates = fields.Str(required=True)
     projects = fields.Str(required=True)
@@ -35,6 +46,11 @@ class ResumeSchema(ma.Schema):
 
 Resume_schema = ResumeSchema()
 Resume_schema = ResumeSchema(many=True)
+
+
+
+
+
 
 
 
